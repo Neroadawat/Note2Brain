4421ee96a12055df1bdc288f514d6703cdcd5738
ixone0 <jasonboy025@gmail.com>
2025-10-09 13:09:28 +0700

    up

---
+++ backend/prisma/migrations/20251009060422_deltimespent/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `timeSpent` on the `FlashcardAttempt` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FlashcardAttempt" DROP COLUMN "timeSpent";
